d2e1fbe3dc1da8a6f28357d4ee7d810caef57ed6
Ania <Ania>
2017-05-03 12:03:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# kodilla_zadanie_9.1
+# Java Script, funkcje, pole trójkąta
---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(10, 17);
+var triangle3Area = getTriangleArea(10, 10);
+
+function getTriangleArea(a, h) {
+    if ( (a <= 0) && (h <= 0) ) {
+        alert ('nieprawidłowe dane, wpisz liczby większe od 0');
+	    console.log('nieprawidłowe dane, wpisz liczby większe od 0');
+	} else {
+		return a*h/2;
+    }
+}
+
+console.log( getTriangleArea(10,6) );
+console.log( triangle1Area );
+console.log( triangle2Area );
+console.log( triangle3Area );
