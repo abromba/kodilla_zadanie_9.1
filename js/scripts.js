fe274caab11c25004b016ae84bf4c3a4bac7fcee
Ania <Ania>
2017-05-06 10:21:08 +0200

    short if, spaces, return

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,11 @@ var triangle2Area = getTriangleArea(10, 3);
 var triangle3Area = getTriangleArea(10, 30);
 
 function getTriangleArea(a, h) {
-    if ( (a <= 0) || (h <= 0) ) {
-        alert ('nieprawidłowe dane, wpisz liczby większe od 0');
-        console.log('nieprawidłowe dane, wpisz liczby większe od 0');
-	} else {
-        return a*h/2;
-    }
+    return a <=0 || h <=0 ? 'Podaj liczbe większą od 0' : a * h / 2;
 }
 
-console.log( getTriangleArea(10,-6) );
+console.log(getTriangleArea(10,-6));
 
-console.log( triangle1Area );
-console.log( triangle2Area );
-console.log( triangle3Area );
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
